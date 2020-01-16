7aa8bf9955983416334f3a9b61d4a21b90cb598f
Łukasz Tomczak <tomczak.luk@gmail.com>
2020-01-16 14:53:15 +0100

    Updated SQL files with new DB schema.

--- SyZaFi/createTableSource.sql
+++ SyZaFi/createTableSource.sql
@@ -1,3 +1,55 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 16 Sty 2020, 14:43
+-- Wersja serwera: 10.4.6-MariaDB
+-- Wersja PHP: 7.3.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `syzafi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `employees`
+--
+
+CREATE TABLE `employees` (
+  `id` int(11) NOT NULL,
+  `firstName` varchar(50) COLLATE utf8_polish_ci NOT NULL,
+  `lastName` varchar(50) COLLATE utf8_polish_ci NOT NULL,
+  `fathersName` varchar(50) COLLATE utf8_polish_ci NOT NULL,
+  `mothersName` varchar(50) COLLATE utf8_polish_ci NOT NULL,
+  `birthday` varchar(50) COLLATE utf8_polish_ci NOT NULL,
+  `placeOfResidence` varchar(500) COLLATE utf8_polish_ci NOT NULL,
+  `correspondenceAddress` varchar(500) COLLATE utf8_polish_ci NOT NULL,
+  `education` varchar(50) COLLATE utf8_polish_ci NOT NULL,
+  `historyOfEmployment` varchar(500) COLLATE utf8_polish_ci NOT NULL,
+  `pesel` int(11) NOT NULL,
+  `childrensNames` varchar(500) COLLATE utf8_polish_ci NOT NULL,
+  `childrensBirthdays` varchar(500) COLLATE utf8_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `users`
+--
+
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `firstName` varchar(30) COLLATE utf8_polish_ci NOT NULL,
@@ -10,3 +62,40 @@ CREATE TABLE `users` (
   `employmentMonth` varchar(30) COLLATE utf8_polish_ci NOT NULL,
   `emailAddress` varchar(30) COLLATE utf8_polish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `employees`
+--
+ALTER TABLE `employees`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `employees`
+--
+ALTER TABLE `employees`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT dla tabeli `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
